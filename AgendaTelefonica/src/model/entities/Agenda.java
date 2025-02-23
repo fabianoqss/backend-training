6cd2168b240b3ee248680f3062745034dc23f770
Fabiano <fabianoquirinos@outlook.com>
2025-02-23 16:18:30 -0300

    :sparkles: feat: Implementações dos métodos na classe Agenda

--- AgendaTelefonica/src/model/entities/Agenda.java
+++ AgendaTelefonica/src/model/entities/Agenda.java
@@ -1,4 +1,35 @@
 package model.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Agenda {
+    private List<Contato> contatos = new ArrayList<>();
+
+    public void addContatos(Contato contato){
+        contatos.add(contato);
+    }
+
+    public void removeContatos(String nome){
+        contatos.remove(nome);
+    }
+
+    public void buscarContatos(String nome){
+        for(Contato c : contatos){
+            if(nome == c.getNome()){
+                System.out.println(
+                        "Nome: " + c.getNome()
+                        + "\n Telefone: " + c.getTelefone()
+                        + "\n Email: " + c.getEmail()
+                        + "\n Tipo do Contato: " + c.getTipoContato()
+                                + "\n Data de Nascimento: " + c.getDataNascimento()
+                                + "\n Endereço: " +  c.getEndereco());
+            }
+        }
+    }
+
+
+    public void listarContato(){
+        
+    }
 }
