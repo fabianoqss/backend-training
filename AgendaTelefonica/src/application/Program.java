8e7d4d3616d60bf410697e696f783981b19260ec
Fabiano <fabianoquirinos@outlook.com>
2025-02-24 15:00:16 -0300

    :sparkles: feat: Implementacao da Classe Program e Agenda

--- AgendaTelefonica/src/application/Program.java
+++ AgendaTelefonica/src/application/Program.java
@@ -1,7 +1,20 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
+        System.out.print("");
 
     }
 }
