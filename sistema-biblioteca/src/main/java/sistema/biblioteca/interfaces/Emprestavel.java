18b871663255c1e267ed675f67bf7340bd1dc8f5
Fabiano <fabianoquirinos@outlook.com>
2025-04-10 16:08:14 -0300

    :sparkles: feat: criacao dos métodos da interface

--- sistema-biblioteca/src/main/java/sistema/biblioteca/interfaces/Emprestavel.java
+++ sistema-biblioteca/src/main/java/sistema/biblioteca/interfaces/Emprestavel.java
@@ -1,4 +1,9 @@
 package sistema.biblioteca.interfaces;
 
+import sistema.biblioteca.model.Livro;
+import sistema.biblioteca.model.Usuario;
+
 public interface Emprestavel {
+    void realizarEmprestimo(Usuario usuario, Livro livro);
+    void registrarDevolucao(Usuario usuario, Livro livro);
 }
